4ebc385aebdd57dd49ae75fda61864fb75d08406
Nestor Padilla <pcu18021379@gmail.com>
2024-02-13 02:13:45 -0600

    a

--- 2-dns/2-dns.py
+++ 2-dns/2-dns.py
@@ -1,7 +1,9 @@
 from colorama import Fore, Back, Style, init
+import os
 
+os.system('clear')
 print(f"{Style.BRIGHT}{Fore.YELLOW}===========================\n\tSERVIDO DNS\n===========================" + Style.RESET_ALL)
-ip = input(f"{Fore.BLUE}1. Ingresa la ip del servidor: {Style.RESET_ALL}")
+ip = input("\u0332".join(f"{Style.BRIGHT}1. Ingresa la ip del servidor: "))
 
 if (ip != "asd"):
-    print(f"{Style.BRIGHT}{Back.RED}Formato incorrecto. Por favor ingresa de nueva la dirección ip{Style.RESET_ALL}")
+    print(f"{Fore.BLACK}{Back.RED}Formato incorrecto. Por favor ingresa de nueva la dirección ip{Style.RESET_ALL}")
